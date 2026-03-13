94244d3bdd26f1a0a7ccec50a3725d3e7c53760a
Kollipara Sri Krishna Venkata Subbarao <103669180+srikrishna0874@users.noreply.github.com>
2026-03-13 21:59:24 +0530

    Project setup commit

---
+++ src/main/java/com/ecommerce/springboot_e_commerce_project/SpringbootECommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.springboot_e_commerce_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootECommerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootECommerceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/springboot_e_commerce_project/SpringbootECommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.springboot_e_commerce_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootECommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
