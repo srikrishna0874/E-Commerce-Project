d4fe33f232d028b69789fc5ec9b3be82793e1c6a
Kamal Chakravarthy <kamalchakravarthy.02@gmail.com>
2026-03-15 19:31:42 +0530

    Added Product Service, Controller & Category Entity, Repo

--- src/main/java/com/ecommerce/springboot_e_commerce_project/controller/ProductController.java
+++ src/main/java/com/ecommerce/springboot_e_commerce_project/controller/ProductController.java
@@ -1,7 +1,66 @@
 package com.ecommerce.springboot_e_commerce_project.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.springboot_e_commerce_project.model.Product;
+import com.ecommerce.springboot_e_commerce_project.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Product>> getProducts(){
+        return new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id){
+        Product p = productService.getProductById(id);
+        System.out.println(p.getProductId());
+        if(p.getProductId() > 0){
+            return new ResponseEntity<>(productService.getProductById(id),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> addProduct(@Valid @RequestBody Product product){
+        Product savedProduct = null;
+
+        try{
+            savedProduct = productService.addProduct(product);
+            return new ResponseEntity<>(savedProduct,HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable int id,@Valid @RequestBody Product product){
+        Product p = productService.updateProduct(id,product);
+
+        if(p.getProductId() == -1)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(p,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Boolean deletedSuccessfully = productService.deleteProduct(id);
+
+        if(deletedSuccessfully)
+            return new ResponseEntity<>("Product Deleted Successfully",HttpStatus.OK);
+
+        return new ResponseEntity<>("Product Not Found",HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/ecommerce/springboot_e_commerce_project/service/ProductService.java
+++ src/main/java/com/ecommerce/springboot_e_commerce_project/service/ProductService.java
@@ -1,19 +1,55 @@
 package com.ecommerce.springboot_e_commerce_project.service;
 
+import com.ecommerce.springboot_e_commerce_project.model.Product;
+import com.ecommerce.springboot_e_commerce_project.repo.ProductRepository;
 import jakarta.persistence.GeneratedValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.processing.Generated;
+import java.util.List;
 
 @Service
 public class ProductService {
 
     @Autowired
-    private ProductRepo productRepo;
+    private ProductRepository productRepo;
 
-    @GeneratedValue
     public List<Product> getProducts() {
-        return
+        return productRepo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productRepo.findById(id).orElse(new Product(-1));
+    }
+
+    public Product addProduct(Product product) {
+        return productRepo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product) {
+        Product existingProduct = productRepo.findById(id).orElse(new Product(-1));
+
+        if(existingProduct.getProductId() == -1) return existingProduct;
+
+        existingProduct.setProductName(product.getProductName());
+        existingProduct.setProductDescription(product.getProductDescription());
+        existingProduct.setProductPrice(product.getProductPrice());
+        existingProduct.setProductImageUrl(product.getProductImageUrl());
+        existingProduct.setProductStockQuantity(product.getProductStockQuantity());
+
+        productRepo.save(existingProduct);
+        return existingProduct;
+    }
+
+    public Boolean deleteProduct(int id) {
+        Product p = productRepo.findById(id).orElse(new Product(-1));
+
+        if(p.getProductId() != -1){
+            productRepo.delete(p);
+            return true;
+        }
+
+        return false;
+
     }
 }
