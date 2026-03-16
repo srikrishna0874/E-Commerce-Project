71bb24d11367dfa0e639ea0ae237a65181ce31d1
Kamal Chakravarthy <kamalchakravarthy.02@gmail.com>
2026-03-17 00:25:11 +0530

    Added Product, Category Integration

--- src/main/java/com/ecommerce/springboot_e_commerce_project/repo/ProductRepository.java
+++ src/main/java/com/ecommerce/springboot_e_commerce_project/repo/ProductRepository.java
@@ -1,10 +1,14 @@
 package com.ecommerce.springboot_e_commerce_project.repo;
 
+import com.ecommerce.springboot_e_commerce_project.model.Category;
 import com.ecommerce.springboot_e_commerce_project.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findAllByCategory(Category category);
 }
