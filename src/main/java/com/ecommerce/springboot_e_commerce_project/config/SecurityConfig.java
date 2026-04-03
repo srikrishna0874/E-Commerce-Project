46586593f55da8d1f020be6e0aabc91d85b1b8d0
Kollipara Sri Krishna Venkata Subbarao <103669180+srikrishna0874@users.noreply.github.com>
2026-04-03 23:43:23 +0530

    Added basic security config

---
+++ src/main/java/com/ecommerce/springboot_e_commerce_project/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ecommerce.springboot_e_commerce_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+
+        httpSecurity
+                .csrf(csrfCustomizer -> csrfCustomizer.disable())
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        return httpSecurity.build();
+    }
+
+}
